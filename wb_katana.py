57f0168d7310d370614e8bd9f6cb6993c5cd5c4d
Heiko Noordhof <h.e.noordhof@rug.nl>
2024-01-22 22:20:29 +0100

    off limits detect, plot markers and color

--- wb_katana.py
+++ wb_katana.py
@@ -2,23 +2,26 @@
 
 import tomllib
 
+max_fuel_liters = 74
+max_fuel_mass = 0.72 * max_fuel_liters
+
+kg_min = 560
+kg_max = 730
+
+arms = [ .250, .270, .290, .310, .330, .350, .370, .390 ]
+
+moment_min = kg_min * arms[0]
+moment_max = kg_max * arms[-1]
+
 def start_diagram():
     import matplotlib.pyplot as plt
 
-    margin = 20
+    margin = 14
 
     color_off_limits = '0.8'
     color_valid_area = 'white'
 
-    kg_min = 560
-    kg_max = 730
-
-    arms = [ .250, .270, .290, .310, .330, .350, .370, .390 ]
-
-    moment_min = kg_min * arms[0]
-    moment_max = kg_max * arms[-1]
-
-    fig, (diagram, info) = plt.subplots(2, 1, figsize=(5,7), height_ratios=(1,1) )
+    fig, (diagram, info) = plt.subplots(2, 1, figsize=(7,7), height_ratios=(3,2) )
 
     info.axis("off")
     info.text(0, 0, '''Aap
@@ -117,6 +120,25 @@ def calc(empty_weight, empty_moment, pob, baggage, fuel=0.0):
     return weight, moment
 
 
+def is_within_limits(weight, moment):
+    print("weight:", weight)
+    if weight < float(kg_min):
+        return False
+    print("moment:", moment)
+    if weight > float(kg_max):
+        return False
+
+    arm = moment / weight 
+    print("arm:", arm)
+    print()
+    if arm < arms[0]:
+        return False
+    if arm > arms[-1]:
+        return False
+
+    return True
+
+
 if __name__ == "__main__":
     profile = load_profile('d-ecpu.toml')
 
@@ -135,8 +157,22 @@ if __name__ == "__main__":
 
     fuel_liters = profile['fuel']
     fuel_weight = fuel_liters * 0.72
-    takeoff_fuel_weight, takeoff_fuel_moment = calc(empty_weight, empty_moment, pob_weight, bagage_weight, fuel_weight)
+    takeoff_weight, takeoff_moment = calc(empty_weight, empty_moment, pob_weight, bagage_weight, fuel_weight)
 
     plt, diagram, info = start_diagram()
-    diagram.plot( [zero_fuel_moment, takeoff_fuel_moment], [zero_fuel_weight, takeoff_fuel_weight], color='green' ) 
+
+    zero_fuel_OK = is_within_limits(zero_fuel_weight, zero_fuel_moment)
+    takeoff_fuel_OK = is_within_limits(takeoff_weight, takeoff_moment)
+    if zero_fuel_OK and takeoff_fuel_OK:
+        plot_color = 'green'
+    else:
+        plot_color = 'red'
+
+    diagram.plot(
+        [ zero_fuel_moment, takeoff_moment ],
+        [ zero_fuel_weight, takeoff_weight ],
+        color=plot_color,
+        marker='.',
+    ) 
+
     plt.show()
